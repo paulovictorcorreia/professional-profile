ab6e846b274bb402aa1e44db07532ee3e63a442e
Paulo Victor Correia <paulocorreiaufrn@gmail.com>
2020-04-14 11:04:37 -0300

    uploading version 1 of the site

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,11 +7,11 @@ class About extends Component {
       var name = this.props.data.name;
       var profilepic= "images/"+this.props.data.image;
       var bio = this.props.data.bio;
-      var street = this.props.data.address.street;
+      //var street = this.props.data.address.street;
       var city = this.props.data.address.city;
       var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
+      //var zip = this.props.data.address.zip;
+      //var phone= this.props.data.phone;
       var email = this.props.data.email;
       var resumeDownload = this.props.data.resumedownload;
     }
@@ -20,7 +20,7 @@ class About extends Component {
       <section id="about">
       <div className="row">
          <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Tim Baker Profile Pic" />
+            <img className="profile-pic"  src={profilepic} alt="Paulo Victor Profile Pic" />
          </div>
          <div className="nine columns main-col">
             <h2>About Me</h2>
@@ -31,10 +31,7 @@ class About extends Component {
                   <h2>Contact Details</h2>
                   <p className="address">
 						   <span>{name}</span><br />
-						   <span>{street}<br />
-						         {city} {state}, {zip}
-                   </span><br />
-						   <span>{phone}</span><br />
+						   <span>{city}, {state},</span><br />
                      <span>{email}</span>
 					   </p>
                </div>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,7 +8,7 @@ class Header extends Component {
       var occupation= this.props.data.occupation;
       var description= this.props.data.description;
       var city= this.props.data.address.city;
-      var networks= this.props.data.social.map(function(network){
+      var networks= this.props.data.social.map(function(network){ 
         return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
       })
     }
@@ -26,7 +26,6 @@ class Header extends Component {
             <li><a className="smoothscroll" href="#about">About</a></li>
 	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
             <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-            <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
             <li><a className="smoothscroll" href="#contact">Contact</a></li>
          </ul>
 
