7221b2e4360b47a971862b54b0b78a80ee260cae
Paulo Victor Correia <paulocorreiaufrn@gmail.com>
2020-04-24 08:53:13 -0300

    Updates

---
+++ precache-manifest.04e344b3b688da49795b15216ce304ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71fb22279886b2ae3dad7b280909f4bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65086100a1bca444abb8",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "639686464763dc9d70c2",
+    "url": "/static/js/2.77e5744a.chunk.js"
+  },
+  {
+    "revision": "6e742f513d9938d07b2d93218f3e4449",
+    "url": "/static/js/2.77e5744a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65086100a1bca444abb8",
+    "url": "/static/js/main.12790d5b.chunk.js"
+  },
+  {
+    "revision": "0dc975abd17d88d50f87",
+    "url": "/static/js/runtime-main.15b696c7.js"
+  }
+]);
